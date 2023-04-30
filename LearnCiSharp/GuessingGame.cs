cdd265d5af95ffcd3738a8c4b568c93071a6b2ee
yuriimacbook <galasayra123213@gmail.com>
2023-04-30 13:01:49 +0300

    add two games

---
+++ LearnCiSharp/GuessingGame.cs
@@ -0,0 +1,52 @@
+﻿namespace Games.GuessingGame;
+
+public class GuessingGame
+{
+    private const int MIN = 0;
+    private const int MAX = 50;
+
+    public static void RunGame()
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(MIN, MAX + 1);
+
+        do
+        {
+            Console.WriteLine($"\nGuess my number! (from {MIN} to {MAX})");
+            Console.Write("Your answer: ");
+
+            int playerAnswer;
+            while (!Int32.TryParse(Console.ReadLine(), out playerAnswer) || !Enumerable.Range(MIN, MAX).Contains(playerAnswer))
+            {
+                Console.WriteLine($"Please, provide only numbers from {MIN} to {MAX}");
+                Console.Write("Your answer: ");
+            }
+
+            if (playerAnswer > secretNumber)
+            {
+                Console.WriteLine("My number is smaller..");
+            }
+            else if (playerAnswer < secretNumber)
+            {
+                Console.WriteLine("My number is bigger..");
+            }
+            else
+            {
+                Console.WriteLine("Bullseye!");
+
+                Console.WriteLine("\nDo you want to play one more time? (type: 'No' or 'N' if you don't want to play with me anymore)");
+                string answer = Console.ReadLine();
+                if (answer.Equals("N") || answer.Equals("No"))
+                {
+                    break;
+                }
+
+                Console.Clear();
+                secretNumber = random.Next(MIN, MAX + 1);
+            }
+        } while (true);
+
+        Console.WriteLine("\nThank you for the game!\nBye!");
+        Task.Delay(10000).Wait();
+    }
+}
---
+++ LearnCiSharp/HangmanGame.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Games.HangmanGame;
+
+public class HangmanGame
+{
+
+    private const int PLAYER_MAX_LIFES = 6;
+    private static readonly string[] _dictionary = { "fish", "dog", "parrot" };
+
+    public static void RunGame()
+    {
+        int currentPlayerLifes = PLAYER_MAX_LIFES;
+
+        int randomIndex = new Random().Next(0, _dictionary.Length);
+        string secretWord = _dictionary[randomIndex];
+
+        char[] letters = secretWord.ToArray();
+        char[] maskedLetters = Enumerable.Repeat('_', secretWord.Length).ToArray();
+        Console.WriteLine(String.Join(" ", maskedLetters));
+
+
+        while (currentPlayerLifes > 0)
+        {
+            string lifeMessage = $"Player has {currentPlayerLifes} life points\n";
+            Console.Write(new string(' ', Console.BufferWidth - lifeMessage.Length) + lifeMessage);
+
+
+            Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+            Console.WriteLine("Please, provide a letter");
+            Console.Write("Your letter: ");
+            char playerAnswer = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if (maskedLetters.Contains(playerAnswer))
+            {
+                Console.WriteLine(String.Join(" ", maskedLetters));
+                Console.WriteLine($"You have already guessed a letter {playerAnswer}");
+                Console.WriteLine("Please, select a letter that has not already been guessed");
+            }
+            else if (letters.Contains(playerAnswer))
+            {
+                List<int> indexes = Enumerable.Range(0, letters.Length).Where(i => letters[i] == playerAnswer).ToList();
+                indexes.ForEach(i => maskedLetters[i] = letters[i]);
+
+                Console.WriteLine(String.Join(" ", maskedLetters));
+                if (letters.SequenceEqual(maskedLetters))
+                {
+                    Console.WriteLine("Congratulations!\nYou win!");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", maskedLetters));
+                currentPlayerLifes--;
+                Console.WriteLine("No such letter\n");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Game over");
+        Task.Delay(10000).Wait();
+    }
+
+}
+
